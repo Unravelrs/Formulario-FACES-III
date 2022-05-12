4fb9a6375ef10d05cec9411637726608e1f54735
Unravelrs <unravel.rs@gmail.com>
2022-05-12 11:39:04 -0500

    agrego variables

--- public/javascripts/sendData.js
+++ public/javascripts/sendData.js
@@ -1,6 +1,18 @@
-function processData() {
+ function processData() {
     var nombre=$("#nombre").val();
-    alert(nombre);
+    var NSS=$("#NSS").val();
+    var primerapellido=$("#primerapellido").val();
+    var segundoapellido=$("#segundoapellido").val();
+    var edad=$("#edad").val();
+    var sexo=$("#sexo").val();
+    var ocupacion=$("#ocupacion").val();
+    var preg1=$("#preg1").val();
+
+
+    
+
+    
+    alert(nombre + " " + primerapellido + " " + segundoapellido + " " + NSS + " " + edad + " " + sexo + " " + ocupacion + " " + preg1);
     $.ajax({
         method: "POST",
         url: "/sendData",
@@ -9,11 +21,13 @@ function processData() {
         },
         cache: false,
         success: function (data) {
-            console.log("datos enviados")
+            console.log("Datos enviados")
 
         },
         error: function (a, b, c) {
             debugger;
         },
     });
+
+
 }
