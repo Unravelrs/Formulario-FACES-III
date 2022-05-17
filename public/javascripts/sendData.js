2656418bd3ae023298e65a1b39ca83d3efca7bd4
egalsan <emmanuel.galeana@abaton.com.mx>
2022-05-17 13:37:59 -0500

    Insersion en BD

--- public/javascripts/sendData.js
+++ public/javascripts/sendData.js
@@ -1,53 +1,95 @@
- function processData() {
-    var nombre=$("#nombre").val();
-    var NSS=$("#NSS").val();
-    var primerapellido=$("#primerapellido").val();
-    var segundoapellido=$("#segundoapellido").val();
-    var edad=$("#edad").val();
-    var sexo=$("#sexo").val();
-    var ocupacion=$("#ocupacion").val();
-    var preg1=$('input[name="preg1"]:checked').val();
-    var preg2=$('input[name="preg2"]:checked').val();
-    var preg3=$('input[name="preg3"]:checked').val();
-    var preg4=$('input[name="preg4"]:checked').val();
-    var preg5=$('input[name="preg5"]:checked').val();
-    var preg6=$('input[name="preg6"]:checked').val();
-    var preg7=$('input[name="preg7"]:checked').val();
-    var preg8=$('input[name="preg8"]:checked').val();
-    var preg9=$('input[name="preg9"]:checked').val();
-    var preg10=$('input[name="preg10"]:checked').val();
-    var preg11=$('input[name="preg11"]:checked').val();
-    var preg12=$('input[name="preg12"]:checked').val();
-    var preg13=$('input[name="preg13"]:checked').val();
-    var preg14=$('input[name="preg14"]:checked').val();
-    var preg15=$('input[name="preg15"]:checked').val();
-    var preg16=$('input[name="preg16"]:checked').val();
-    var preg17=$('input[name="preg17"]:checked').val();
-    var preg18=$('input[name="preg18"]:checked').val();
-    var preg19=$('input[name="preg19"]:checked').val();
-    var preg20=$('input[name="preg20"]:checked').val();
+function processData() {
+    var nombre = $("#nombre").val();
+    var nss = $("#NSS").val();
+    var primerapellido = $("#primerapellido").val();
+    var segundoapellido = $("#segundoapellido").val();
+    var edad = $("#edad").val();
+    var sexo = $("#sexo").val();
+    var ocupacion = $("#ocupacion").val();
+    var preg1 = $('input[name="preg1"]:checked').val();
+    var preg2 = $('input[name="preg2"]:checked').val();
+    var preg3 = $('input[name="preg3"]:checked').val();
+    var preg4 = $('input[name="preg4"]:checked').val();
+    var preg5 = $('input[name="preg5"]:checked').val();
+    var preg6 = $('input[name="preg6"]:checked').val();
+    var preg7 = $('input[name="preg7"]:checked').val();
+    var preg8 = $('input[name="preg8"]:checked').val();
+    var preg9 = $('input[name="preg9"]:checked').val();
+    var preg10 = $('input[name="preg10"]:checked').val();
+    var preg11 = $('input[name="preg11"]:checked').val();
+    var preg12 = $('input[name="preg12"]:checked').val();
+    var preg13 = $('input[name="preg13"]:checked').val();
+    var preg14 = $('input[name="preg14"]:checked').val();
+    var preg15 = $('input[name="preg15"]:checked').val();
+    var preg16 = $('input[name="preg16"]:checked').val();
+    var preg17 = $('input[name="preg17"]:checked').val();
+    var preg18 = $('input[name="preg18"]:checked').val();
+    var preg19 = $('input[name="preg19"]:checked').val();
+    var preg20 = $('input[name="preg20"]:checked').val();
 
-   
-    var respares = ((parseInt(preg2)) + parseInt(preg4) + parseInt(preg6) + parseInt(preg8) + parseInt (preg10) + parseInt (preg12) + parseInt(preg14) + parseInt(preg16) + parseInt(preg18) + parseInt(preg20));
-    var resnones = ((parseInt(preg1)) + parseInt(preg3) + parseInt(preg5) + parseInt(preg7) + parseInt(preg9) + parseInt(preg11) + parseInt(preg13) + parseInt(preg15) + parseInt(preg17) + parseInt(preg19));
-    
-    
-    alert(nombre + " " + primerapellido + " " + segundoapellido + " " + NSS + " " + edad + " " + sexo + " " + ocupacion + " " + respares + " " + resnones);
+    var respares =
+        parseInt(preg2) +
+        parseInt(preg4) +
+        parseInt(preg6) +
+        parseInt(preg8) +
+        parseInt(preg10) +
+        parseInt(preg12) +
+        parseInt(preg14) +
+        parseInt(preg16) +
+        parseInt(preg18) +
+        parseInt(preg20);
+    var resnones =
+        parseInt(preg1) +
+        parseInt(preg3) +
+        parseInt(preg5) +
+        parseInt(preg7) +
+        parseInt(preg9) +
+        parseInt(preg11) +
+        parseInt(preg13) +
+        parseInt(preg15) +
+        parseInt(preg17) +
+        parseInt(preg19);
+
+    alert(
+        nombre +
+            " " +
+            primerapellido +
+            " " +
+            segundoapellido +
+            " " +
+            NSS +
+            " " +
+            edad +
+            " " +
+            sexo +
+            " " +
+            ocupacion +
+            " " +
+            respares +
+            " " +
+            resnones
+    );
+
     $.ajax({
         method: "POST",
         url: "/sendData",
         data: {
-           
+            nombre: nombre,
+            nss: nss,
+            apellido1: primerapellido,
+            apellido2: segundoapellido,
+            edad: edad,
+            sexo: sexo,
+            ocupacion: ocupacion,
+            respares: respares,
+            resnones: resnones,
         },
         cache: false,
         success: function (data) {
-            console.log("Datos enviados")
-
+            alert(data);
         },
         error: function (a, b, c) {
             debugger;
         },
     });
-
-
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const { dac } = require("common");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +9,17 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/sendData', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/sendData', async function(req, res, next) {
+  let data=req.body;
+  try {
+    let resp=await dac.response.insert(data)
+
+    res.json("Datos insertados correctamente");
+  } catch (error) {
+    res.json(error);
+  }
+ 
+  
 });
 
 module.exports = router;
